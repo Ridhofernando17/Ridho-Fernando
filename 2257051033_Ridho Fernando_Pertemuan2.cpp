21795baf267916a95001f91b20de809b9e947865
Ridhofernando17 <112592020+Ridhofernando17@users.noreply.github.com>
2022-09-11 21:15:07 +0700

    Latihan DDP 2

---
+++ 2257051033_Ridho Fernando_Pertemuan2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float phi = 3.14;
+	float r = 7;
+	float L = phi*r*r;
+	
+	cout<<"Menghitung luas LIngkaran"<<r<<"adalah"<<L;
+	return 0;
+}
